31455b1bcb2ffcb66253f60641b72127c16050aa
divne7022 <www.divinehome12@gmail.com>
2025-08-07 14:31:46 +0000

    enhance met downscaling with adding a support of soil temp, RH, soil moisture, PPFD and refactored to address other critical issues

--- modules/data.atmosphere/R/met_temporal_downscale.Gaussian_ensemble.R
+++ modules/data.atmosphere/R/met_temporal_downscale.Gaussian_ensemble.R
@@ -29,7 +29,7 @@ substrRight <- function(x, n) {
 met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfolder, 
                                                      input_met, train_met, overwrite = FALSE, verbose = FALSE, 
                                                      swdn_method = "sine", n_ens = 10, w_len = 20, utc_diff = -6, ... ) {
-
+  
   sub_str <- substrRight(input_met, 7)
   year <- substr(sub_str, 1, 4)
   year <- as.numeric(year)
@@ -38,9 +38,11 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
   # Variable names
   var <- data.frame(CF.name <- c("air_temperature", "air_temperature_max", "air_temperature_min", 
                                  "surface_downwelling_longwave_flux_in_air", "air_pressure", "surface_downwelling_shortwave_flux_in_air", 
-                                 "eastward_wind", "northward_wind", "specific_humidity", "precipitation_flux"), 
-                    units <- c("Kelvin", "Kelvin", "Kelvin", "W/m2", "Pascal", "W/m2", "m/s", 
-                               "m/s", "g/g", "kg/m2/s"))
+                                 "eastward_wind", "northward_wind", "specific_humidity", "precipitation_flux",
+                                 "soil_temperature", "relative_humidity", "volume_fraction_of_condensed_water_in_soil",
+                                 "surface_downwelling_photosynthetic_photon_flux_in_air"), 
+                    units <- c("Kelvin", "Kelvin", "Kelvin", "W m-2", "Pascal", "W m-2", "m/s", 
+                               "m/s", "kg/kg", "kg m-2 s-1", "Kelvin", "%", "1", "umol m-2 s-1"))
   # Reading in the training data
   train <- list()
   tem <- ncdf4::nc_open(train_met)
@@ -100,6 +102,10 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
     sp <- 365
   }
   
+  damping_factor <- 0.7  # Soil temperature variations are ~70% of air temperature
+  phase_lag_hours <- 3   # Soil temperature lags by ~3 hours at 5cm depth
+  
+  
   # Now we start a for loop for the ensemble members and begin downscaling. A
   # random normal distribution is used to downscale as so;
   # (mean <- value of source data) (sd <- +/- window_days of train data at the
@@ -141,14 +147,100 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
       len_diff <- reso_len - length(tem.met)
       tem.met <- append(tem.met,values = rep(NA,len_diff)) 
     } else {
-        for (x in seq(from=0, to=reso_len, by=div)){
-          tem.met[x] <- sourtemp[x / div]
-        }
+      for (x in seq(from=0, to=reso_len, by=div)){
+        tem.met[x] <- sourtemp[x / div]
+      }
     }
     
     spline.temp = zoo::na.spline(tem.met)
     df[1:reso_len, "air_temperature"] <- spline.temp
     
+    # air temperature max and min downscaling with Gaussian ensemble
+    if (all(is.na(source$air_temperature_max)) || all(is.na(source$air_temperature_min))) {
+      daily_temps <- split(df$air_temperature, ceiling(seq_along(df$air_temperature)/(24/reso)))
+      daily_max <- sapply(daily_temps, max)
+      daily_min <- sapply(daily_temps, min)
+      
+      df$air_temperature_max <- rep(daily_max, each = 24/reso)
+      df$air_temperature_min <- rep(daily_min, each = 24/reso)
+    } else {
+      temp_max <- vector()
+      temp_min <- vector()
+      
+      for (x in seq_along(source$air_temperature_max)) {
+        lowday <- (x - w_len) * div
+        highday <- (x + w_len) * div
+        if (lowday < 0) {
+          lowday <- 0
+        }
+        if (highday > reso_len) {
+          highday <- reso_len
+        }
+        
+        if (!is.na(source$air_temperature_max[x])) {
+          dwnsc_max <- vector()
+          for (n in seq_len(div)) {
+            dwnsc_max[n] <- stats::rnorm(1, 
+                                         mean = source$air_temperature_max[x], 
+                                         sd = stats::sd(train$air_temperature_max[lowday:highday], na.rm = TRUE))
+          }
+          temp_max <- append(temp_max, dwnsc_max)
+        }
+        
+        if (!is.na(source$air_temperature_min[x])) {
+          dwnsc_min <- vector()
+          for (n in seq_len(div)) {
+            dwnsc_min[n] <- stats::rnorm(1, 
+                                         mean = source$air_temperature_min[x], 
+                                         sd = stats::sd(train$air_temperature_min[lowday:highday], na.rm = TRUE))
+          }
+          temp_min <- append(temp_min, dwnsc_min)
+        }
+      }
+      df$air_temperature_max <- temp_max[1:reso_len]
+      df$air_temperature_min <- temp_min[1:reso_len]
+    }
+    df$air_temperature_max <- pmax(df$air_temperature_max, df$air_temperature, na.rm = TRUE)
+    df$air_temperature_min <- pmin(df$air_temperature_min, df$air_temperature, na.rm = TRUE)
+    
+    # soil temperature
+    soursoiltemp <- source$soil_temperature
+    if (!all(is.na(soursoiltemp))) {
+      soil.met <- vector()
+      
+      if (length(soursoiltemp) <= 366) {
+        for (i in seq_along(soursoiltemp)) {
+          soil.met <- append(soil.met, rep(soursoiltemp[i], div))
+        }
+      } else {
+        for (x in seq(from=0, to=reso_len, by=div)) {
+          soil.met[x] <- soursoiltemp[x / div]
+        }
+      }
+      lag_step <- round(phase_lag_hours / reso)
+      tair_mean <- mean(df$air_temperature, na.rm = TRUE)
+      soil_base <- mean(soursoiltemp, na.rm = TRUE)
+      if (lag_step > 0 && lag_step < length(df$air_temperature)) {
+        tair_lag <- c(rep(tair_mean, lag_step), 
+                      df$air_temperature[1:(length(df$air_temperature) - lag_step)])
+      } else {
+        tair_lag <- df$air_temperature
+      }
+      soil_proc <- soil_base + 
+        damping_factor * (tair_lag - tair_mean)
+      
+      if (!all(is.na(train$soil_temperature))) {
+        soil_residual_sd <- sd(train$soil_temperature - train$air_temperature, na.rm = TRUE)
+        if (!is.na(soil_residual_sd) && soil_residual_sd > 0) {
+          soil_noise <- rnorm(length(soil_proc), 0, soil_residual_sd * 0.5)
+          soil_proc <- soil_proc + soil_noise
+        }
+      }
+      
+      df[1:reso_len, "soil_temperature"] <- soil_proc[1:reso_len]
+    } else {
+      df[1:reso_len, "soil_temperature"] <- rep(NA, reso_len)
+    }
     # after this maybe we can run it through the random norm to add variation
     # but not sure how models will react 
     
@@ -156,7 +248,7 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
     # a total possible amount of precip.  It randomly distributes the values of
     # precipitation
     rand_vect_cont <- function(N, M, sd = 1) {
-      vec <- truncnorm::rtruncnorm(N, a = 0, b = Inf,M/N, sd)
+      vec <- truncnorm::rtruncnorm(N, a = 0, b = Inf, M/N, sd)
       vec/sum(vec) * M
     }
     precip <- vector()
@@ -177,9 +269,8 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
     }
     df$precipitation_flux <- precip
     
-    # Specific Humidity, eastward wind and northward wind
     wnd <- c("specific_humidity", "eastward_wind", "northward_wind", "surface_downwelling_longwave_flux_in_air", 
-             "air_pressure")
+             "air_pressure", "relative_humidity", "volume_fraction_of_condensed_water_in_soil")
     for (u in wnd) {
       train_vec <- vector()
       a <- as.numeric(train[[u]])
@@ -199,7 +290,57 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
           }
           dwnsc_day <- vector()
           for (n in seq_len(div)) {
-            dwnsc_day[n] <- stats::rnorm(1, mean = sour[x], sd = stats::sd(a[lowday:highday]))
+            if (u == "volume_fraction_of_condensed_water_in_soil") {
+              idx <- (x - 1) * div + n
+              current_precip <- ifelse(idx <= length(precip) && idx >= 1, 
+                                       precip[idx], 0)
+              if (is.na(current_precip)) current_precip <- 0
+              antecedent_sm <- ifelse(idx > 1, 
+                                      sour[max(1, x-1)], 
+                                      sour[x])
+              base_sd <- stats::sd(a[lowday:highday], na.rm = TRUE)
+              sm_cv <- sd(sour, na.rm = TRUE) / mean(sour, na.rm = TRUE)
+              if (!is.na(sm_cv) && is.finite(sm_cv)) {
+                sm_fc <- quantile(sour, 0.75, na.rm = TRUE)
+                moisture_stress <- abs(antecedent_sm - sm_fc) / sm_fc
+                uncertainty_factor <- 1.0 + sm_cv * moisture_stress
+                if (current_precip > 0) {
+                  uncertainty_factor <- uncertainty_factor * 1.2  # 20% increase during precipitation
+                }
+              } else {
+                uncertainty_factor <- 1.0
+              }
+              uncertainty_factor <- pmax(0.7, pmin(uncertainty_factor, 1.8))
+              sd_adj <- base_sd * uncertainty_factor
+              soil_moisture <- stats::rnorm(1, mean = sour[x], sd = sd_adj)
+              dwnsc_day[n] <- pmax(0, pmin(soil_moisture, 1.0))
+            } else if (u == "relative_humidity") {
+              base_sd <- stats::sd(a[lowday:highday], na.rm = TRUE)
+              if (is.na(base_sd) || base_sd <= 0) {
+                base_sd <- 5.0  
+              }
+              temp_idx <- (x - 1) * div + n
+              if (temp_idx > 0 && temp_idx <= length(df$air_temperature) && x <= length(source$air_temperature)) {
+                current_temp_c <- df$air_temperature[temp_idx] - 273.15
+                source_temp_c <- source$air_temperature[x] - 273.15
+                
+                if (current_temp_c > -40 && current_temp_c < 50 && source_temp_c > -40 && source_temp_c < 50) {
+                  # magnus formula for saturation vapor pressure (kPa)
+                  es_current <- 0.61078 * exp((17.27 * current_temp_c) / (current_temp_c + 237.3))
+                  es_source <- 0.61078 * exp((17.27 * source_temp_c) / (source_temp_c + 237.3))
+                  saturation_ratio <- es_source / es_current
+                  adjusted_rh <- sour[x] * saturation_ratio
+                } else {
+                  adjusted_rh <- sour[x]
+                }
+              } else {
+                adjusted_rh <- sour[x]
+              }
+              downscaled_rh <- stats::rnorm(1, mean = adjusted_rh, sd = base_sd)
+              dwnsc_day[n] <- pmax(0, pmin(100, downscaled_rh))
+            } else {
+              dwnsc_day[n] <- stats::rnorm(1, mean = sour[x], sd = stats::sd(a[lowday:highday], na.rm = TRUE))
+            }
           }
           train_vec <- append(train_vec, dwnsc_day)
         }
@@ -208,6 +349,13 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
     }
     
     df$specific_humidity[df$specific_humidity < 0] <- 0
+    
+    if ("volume_fraction_of_condensed_water_in_soil" %in% names(df)) {
+      df$volume_fraction_of_condensed_water_in_soil[
+        df$volume_fraction_of_condensed_water_in_soil < 0] <- 0
+      df$volume_fraction_of_condensed_water_in_soil[
+        df$volume_fraction_of_condensed_water_in_soil > 1] <- 1
+    }
     
     # Downwelling shortwave radiation flux Ephemeris is a function to calculate
     # sunrise/sunset times and daylength for SW calculations in sine swdn_method
@@ -257,10 +405,11 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
         
         # swdn = 0 without sunlight
         srs <- eph$sunrise
-        hr <- substr(srs[i], 1, 2)
-        hr <- as.numeric(hr)
+        srs_hr <- floor(srs[i] / 100)  # extract hours (works for both 430 -> 4 and 1215 -> 12)
+        srs_min <- (srs[i] %% 100) / 60  # convert minutes to fraction (30 -> 0.5)
         # utc_diff must be used so we can begin the sine wave at local sunrise
-        hr <- hr + utc_diff
+        hr <- srs_hr + srs_min + utc_diff
+        hr <- max(0, min(23, hr))
         
         l <- vector()
         for (n in seq_len(hr)) {
@@ -269,7 +418,7 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
         for (n in seq_along(wav)) {
           l[n + hr] <- wav[n]
         }
-        for (n in seq_len(24 - (length(wav) + hr))) {
+        for (n in seq_len(floor(24 - (length(wav) + hr)))) {
           l[n + hr + length(wav)] <- 0
         }
         
@@ -307,8 +456,8 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
                   as.POSIXct(paste0(eph_year, "-12-31 18:00:00"),tz="UTC"), 
                   by = inter)
       days.doy <- as.numeric(format(days,"%j"))
-      days.hour <- lubridate::hour(days) + lubridate::minute(days) / 60 + lubridate::second(days) / 3600
-      cosZ <- PEcAn.data.atmosphere::cos_solar_zenith_angle(days.doy, lat_train, lon_train, inter, days.hour)
+      days.hour <- as.numeric(lubridate::hour(days) + lubridate::minute(days) / 60 + lubridate::second(days) / 3600)
+      cosZ <- PEcAn.data.atmosphere::cos_solar_zenith_angle(days.doy, lat_train, lon_train, reso*3600, days.hour)
       I <- 1000 * cosZ
       m <- vector()
       for (i in seq_len(12)) {
@@ -327,13 +476,19 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
       C <- 0.7
       hdry <- vector()
       for (i in seq_along(precip)) {
-        if (precip[i] > 0) {
-          p <- 0.65
+        if (i <= length(df$air_temperature_max) && 
+            i <= length(df$air_temperature_min) && 
+            i <= length(bmlist)) {
+          p <- ifelse(precip[i] > 0, 0.65, 2)
+          if (!is.na(df$air_temperature_max[i]) && !is.na(df$air_temperature_min[i])) {
+            temp_range <- df$air_temperature_max[i] - df$air_temperature_min[i]
+            hdry[i] <- A * p * (1 - exp(-1 * bmlist[i] * (temp_range^C)))
+          } else {
+            hdry[i] <- 0
+          }
+        } else {
+          hdry[i] <- 0
         }
-        if (precip[i] == 0) {
-          p <- 2
-        }
-        hdry[i] <- A * p * (1 - exp(-1 * bmlist[i] * ((temp_max[i] - temp_min[i])^C)))
       }
       hdry[hdry < 0] <- 0
       swflux <- hdry * I
@@ -342,10 +497,70 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
     # Waichler method is the only method with ensembles for downwelling shortwave flux
     
     df$surface_downwelling_shortwave_flux_in_air <- swflux
-    # Will need to change below if we figure out how to downscale this
-    df$air_temperature_max <- rep(NA, reso_len)
-    df$air_temperature_min <- rep(NA, reso_len)
     
+    # PPFD downscaling
+    ppfd_source <- source$surface_downwelling_photosynthetic_photon_flux_in_air
+    if (all(is.na(ppfd_source))) {
+      ppfd_flux <- swflux * 0.45 * 4.57  # PAR fraction × umol/J conversion
+      ppfd_flux[ppfd_flux < 0] <- 0
+      df$surface_downwelling_photosynthetic_photon_flux_in_air <- ppfd_flux
+    } else {
+      inter <- paste0(reso, " hour")
+      days <- seq(as.POSIXct(paste0(year, "-01-01 00:00:00"), tz = "UTC"), 
+                  length.out = reso_len, by = inter)
+      days.doy <- as.numeric(format(days, "%j"))
+      days.hour <- as.numeric(format(days, "%H")) + as.numeric(format(days, "%M"))/60
+      cosZ <- PEcAn.data.atmosphere::cos_solar_zenith_angle(days.doy, lat_train, lon_train, 
+                                                            reso*3600, days.hour)
+      is_daylight <- cosZ > 0
+      
+      train_ppfd <- train$surface_downwelling_photosynthetic_photon_flux_in_air
+      if (all(is.na(train_ppfd)) && !all(is.na(train$surface_downwelling_shortwave_flux_in_air))) {
+        train_ppfd <- train$surface_downwelling_shortwave_flux_in_air * 0.45 * 4.57
+      }
+      
+      train_vec <- vector()
+      for (x in seq_along(ppfd_source)) {
+        lowday <- (x - w_len) * div
+        highday <- (x + w_len) * div
+        if (lowday < 0) {
+          lowday <- 0
+        }
+        if (highday > reso_len) {
+          highday <- reso_len
+        }
+        if (length(train_ppfd) >= highday && !all(is.na(train_ppfd[lowday:highday]))) {
+          base_sd <- sd(train_ppfd[lowday:highday], na.rm = TRUE)
+        } else {
+          base_sd <- NA
+        }
+        if (is.na(base_sd) || base_sd < 1e-6) {
+          cv_ppfd <- sd(ppfd_source, na.rm = TRUE) / mean(ppfd_source, na.rm = TRUE)
+          if (!is.na(cv_ppfd) && is.finite(cv_ppfd)) {
+            base_sd <- cv_ppfd * abs(ppfd_source[x])
+          } else {
+            base_sd <- 0.15 * abs(ppfd_source[x])
+          }
+        }
+        
+        dwnsc_day <- vector()
+        for (n in seq_len(div)) {
+          idx <- (x - 1) * div + n
+          if (idx > 0 && idx <= length(is_daylight) && is_daylight[idx]) {
+            dwnsc_day[n] <- max(0, rnorm(1, mean = ppfd_source[x], sd = base_sd)) # daytime using gaussian downscaling
+          } else {
+            dwnsc_day[n] <- 0 # nighttime ppfd must be zero
+          }
+        }
+        train_vec <- c(train_vec, dwnsc_day)
+      }
+      df$surface_downwelling_photosynthetic_photon_flux_in_air <- train_vec[1:reso_len]
+    }
+    df$surface_downwelling_photosynthetic_photon_flux_in_air[
+      df$surface_downwelling_photosynthetic_photon_flux_in_air < 0] <- 0
+    # maximum PPFD is ~2500 umol m-2 s-1 under full sunlight
+    df$surface_downwelling_photosynthetic_photon_flux_in_air[
+      df$surface_downwelling_photosynthetic_photon_flux_in_air > 2500] <- 2500
     
     # Putting all the variables together in a data frame
     downscaled.met <- data.frame(df)
@@ -366,13 +581,14 @@ met_temporal_downscale.Gaussian_ensemble <- function(in.path, in.prefix, outfold
     
     rows <- 1
     dir.create(outfolder, showWarnings = FALSE, recursive = TRUE)
-
-    loc.file <- file.path(outfolder, paste0(source_name, ".dwnsc.gauss.ens", 
+    source_name <- basename(input_met)  # extracts "US-Ha1.2004.nc" from full path
+    loc.file <- file.path(outfolder, paste0(tools::file_path_sans_ext(source_name), ".dwnsc.gauss.ens", 
                                             e, ".", year, ".nc"))
     
-    loc <- ncdf4::nc_create(filename = loc.file, vars = train.list, verbose = verbose)
+    loc <- ncdf4::nc_create(filename = loc.file, vars = train.list, force_v4 = TRUE, verbose = verbose)
     for (j in seq_along(var$CF.name)) {
-      ncdf4::ncvar_put(nc = loc, varid = as.character(var$CF.name[j]), vals = downscaled.met[[j]])
+      var_name <- as.character(var$CF.name[j])
+      ncdf4::ncvar_put(nc = loc, varid = var_name, vals = downscaled.met[[var_name]])
     }
     ncdf4::nc_close(loc)
     
