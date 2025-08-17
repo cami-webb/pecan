fa09803882e77ddf78d3c2eba1e530b6138c6800
Blesson <blessonthomas2005@gmail.com>
2025-08-17 17:49:59 +0530

    Update ensemble.R

--- modules/uncertainty/R/ensemble.R
+++ modules/uncertainty/R/ensemble.R
@@ -163,7 +163,8 @@ get.ensemble.samples <- function(ensemble.size, pft.samples, env.samples,
            same.i <- sample(nrow(pft.samples[[pft.i]][[1]]), ensemble.size, replace = TRUE)
         }
         else {
-            stop("Sampling method ", method, "is not recognized")}
+            stop("Sampling method ", method, "is not recognized")
+        }
         
       }
       
