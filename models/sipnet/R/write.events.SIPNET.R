8fd9f810c71bd4cf62fa5d845d7211dd14f55262
David LeBauer <dlebauer@gmail.com>
2025-09-24 21:52:04 -0700

    Apply suggestion from @dlebauer

--- models/sipnet/R/write.events.SIPNET.R
+++ models/sipnet/R/write.events.SIPNET.R
@@ -44,6 +44,7 @@
 write.events.SIPNET <- function(events_json, outdir) {
     # TODO add overwrite argument
     x <- jsonlite::fromJSON(events_json, simplifyVector = FALSE)
+    # allow a single site events.json that does not have a site_id 
     site_objs <- if (!is.null(x$site_id)) list(x) else x
     files_written <- vector()
 
