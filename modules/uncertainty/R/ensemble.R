bf3a1c93088f886b8777215eb4619adebeb9143c
Chris Black <chris@ckblack.org>
2025-11-20 14:38:43 -0800

    relicense PEcAn.uncertainty as BSD3 with agreement of all contributors

--- modules/uncertainty/R/ensemble.R
+++ modules/uncertainty/R/ensemble.R
@@ -1,12 +1,3 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2012 University of Illinois, NCSA.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the 
-# University of Illinois/NCSA Open Source License
-# which accompanies this distribution, and is available at
-# http://opensource.ncsa.illinois.edu/license.html
-#-------------------------------------------------------------------------------
-
 ##' Reads output from model ensemble
 ##'
 ##' Reads output for an ensemble of length specified by \code{ensemble.size} and bounded by \code{start.year} 
