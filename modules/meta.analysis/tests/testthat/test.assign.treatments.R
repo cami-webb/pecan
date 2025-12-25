0b041d39d2cc4f5f8b7f47cd92412eb2d7352b2e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2025-12-25 13:46:24 -0500

    Move assign.treatments test to meta-analysis

--- modules/meta.analysis/tests/testthat/test.assign.treatments.R
+++ modules/meta.analysis/tests/testthat/test.assign.treatments.R
@@ -5,7 +5,7 @@ test_that("`assign.treatments` correctly assigns control treatment", {
     control = c(1, 0, 0, 1, 0, 0),
     trt_id = NA
   )
-  
+
   updated_data <- assign.treatments(data)
   expect_equal(updated_data$trt_id, c("control", NA, NA, "control", "control", "control"))
 })
@@ -19,15 +19,4 @@ test_that("`assign.treatments` gives an error if no control treatment is set for
   )
 
   expect_error(assign.treatments(data), "No control treatment set")
-})
-
-test_that("`drop.columns` able to drop specified columns from data", {
-  data <- data.frame(
-    id = c(1, 2, 3),
-    name = c("a", "b", "c"),
-    value = c(1.2, 4.5, 6.7)
-  )
-
-  updated_data <- drop.columns(data, c("name", "not_a_column"))
-  expect_equal(colnames(updated_data), c("id", "value"))
 })
