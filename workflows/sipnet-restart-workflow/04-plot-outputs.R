0df2bbc9fe7bd912ab7103cfddeea851097ffd6e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2026-04-05 14:37:16 -0400

    update plotting code

--- workflows/sipnet-restart-workflow/04-plot-outputs.R
+++ workflows/sipnet-restart-workflow/04-plot-outputs.R
@@ -1,10 +1,8 @@
 #!/usr/bin/env Rscript
 
-config <- config::get(file = "modules/data.land/inst/sipnet-restart-workflow/config.yml")
+library(ggplot2)
 
-outdir <- file.path(config$outdir_root, "out")
-ncfiles <- list.files(outdir, full.names = TRUE)
-nc <- ncdf4::nc_open(ncfiles[[1]])
+config <- config::get(file = "workflows/sipnet-restart-workflow/config.yml")
 
 outdir_root <- config[["outdir_root"]]
 events_json_file <- fs::path(outdir_root, "events.json")
@@ -15,33 +13,39 @@ events_df <- dplyr::bind_rows(events[[1]][["events"]]) |>
   dplyr::filter(.data$event_type != "irrigation") |>
   dplyr::arrange(.data$date)
 
-events_df |>
-  dplyr::select("event_type":"crop_code")
+modeloutdir <- file.path(config$outdir_root, "output", "out")
+runids <- list.files(modeloutdir)
 
-results <- PEcAn.utils::read.output(
-  ncfiles = ncfiles,
-  variables = c("NEE", "LAI", "AGB", "TotSoilCarb"),
-  dataframe = TRUE
-) |>
-  dplyr::as_tibble()
+read_output <- function(modeloutdir, runid) {
+  PEcAn.utils::read.output(
+    runid,
+    file.path(modeloutdir, runid),
+    variables = c("NEE", "LAI", "AGB", "TotSoilCarb"),
+    dataframe = TRUE
+  ) |>
+    dplyr::mutate(run_id = .env$runid) |>
+    dplyr::as_tibble()
+}
 
-library(ggplot2)
+results <- purrr::map(runids, read_output, modeloutdir = modeloutdir) |>
+  dplyr::bind_rows()
+
 plt <- results |>
   tidyr::pivot_longer(
-    -c("posix", "year"),
+    -c("posix", "year", "run_id"),
     names_to = "variable",
     values_to = "value"
   ) |>
   ggplot() +
-  aes(x = posix, y = value) +
+  aes(x = posix, y = value, color = run_id) +
   geom_line() +
   geom_vline(
-    aes(xintercept = date, color = event_type),
-    data = events_df,
-    linetype = "dashed"
+    aes(xintercept = date, linetype = event_type),
+    data = events_df
   ) +
   facet_wrap(vars(variable), scales = "free") +
   theme_bw() +
   theme(legend.position = "bottom")
+plt
 
 ggsave("~/Pictures/restarts.png", plt, width = 12, height = 9, units = "in")
